530b554686d06dbfbec2bdea1d168b92076cf345
Mazba Turza <turza1994@yahoo.com>
2022-02-13 21:34:21 +0600

    configure redux for manufacturers, setting layout for navbar and sidebar2

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -20,29 +20,13 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mx-auto">
                         <li className="nav-item mx-2">
-                            <Link className="nav-link active text-black" aria-current="page" href="/">Home</Link>
+                            <Link aria-current="page" href="/">Home</Link>
                         </li>
                         <li className="nav-item mx-2">
-                            <Link className="nav-link active text-black" aria-current="page" href="/">All Products</Link>
+                            <Link aria-current="page" href="/">All Products</Link>
                         </li>
                         <li className="nav-item mx-2">
-                            <Link className="nav-link active text-black" aria-current="page" href="/">Cart</Link>
-                        </li>
-                    </ul>
-
-                    {/* <form className="ms-auto d-flex justify-content-center">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form> */}
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active text-black" href="/"><i className="fas fa-search fs-6 mx-2 fst-italic"></i></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active text-black" href="/"><i className="fas fa-shopping-cart fs-6 mx-2 fst-italic"></i></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active text-black" href="/"><i className="far fa-user fs-6 mx-2 fst-italic"></i></Link>
+                            <Link aria-current="page" href="/">Cart</Link>
                         </li>
                     </ul>
                 </div>
